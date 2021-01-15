a4859e1f97039228482440c03675bcb9d855a862
Tuguldur <apoxmn@gmail.com>
2021-01-15 16:59:50 +0800

    Point saving and Game ending

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var points = [ 0 , 0];
 var turnPoint = 0 ;
 // Dice point random 1-6
 var diceNumber = Math.floor( Math.random()* 6)+ 1;
-window.document.getElementById('score-0').textContent = '0' ;
-window.document.getElementById('score-1').textContent = '0' ;
-window.document.getElementById('current-0').textContent = '0' ;
-window.document.getElementById('current-1').textContent = '0';
-window.document.querySelector(".dice").style.display = "none";
+document.getElementById('score-0').textContent = '0' ;
+document.getElementById('score-1').textContent = '0' ;
+document.getElementById('current-0').textContent = '0' ;
+document.getElementById('current-1').textContent = '0';
+document.querySelector(".dice").style.display = "none";
 document.querySelector('.btn-roll').addEventListener("click" , function()
 {
     var diceNumber = Math.floor( Math.random()* 6)+ 1;
@@ -23,21 +23,51 @@ if(diceNumber !== 1)
     document.getElementById('current-' + activeP).textContent = turnPoint ;
 }else
 {       
-        turnPoint = 0 ;
-        document.getElementById('current-' + activeP).textContent = 0 ;
-        if(activeP === 0)
-        {
-            activeP = 1 ;
-        }else 
-        {
-            activeP = 0 ;
-        }
-        document.querySelector(".player-0-panel").classList.toggle("active");
-        document.querySelector(".player-1-panel").classList.toggle("active");
-        document.querySelector(".dice").style.display = "none";
+        switchP();
         
 }
     }
     );
+    // Saving turn point
+        document.querySelector(".btn-hold").addEventListener("click", function()
+        { 
+        //  if(activeP === 0)
+        //     {
+        //         points[0] = points[0] + turnPoint ;
+        //     }else
+        //     {
+        //         points[1] = points[1] + turnPoint ;
+        //     }
+            points[activeP] = points[activeP] + turnPoint ;
+            document.getElementById('score-' + activeP).textContent = points[activeP] ;
+            // win
+            if(points[activeP] >= 20)
+            {
+                document.getElementById("name-" + activeP).textContent = "WINNER !!! ";
+                document.querySelector(".player-" +activeP+ "-panel").classList.add('winner');
+            }else
+            {
+                
+                // switch turn
+                switchP();
+            }
 
+        }
+        );
+        function switchP()
+        {   
+            turnPoint = 0 ;
+            document.getElementById('current-' + activeP).textContent = 0 ;
+            if(activeP === 0)
+            {
+                activeP = 1 ;
+            }else 
+            {
+                activeP = 0 ;
+            }
+            document.querySelector(".player-0-panel").classList.toggle("active");
+            document.querySelector(".player-1-panel").classList.toggle("active");
+            document.querySelector(".dice").style.display = "none";
+
+        };
 
